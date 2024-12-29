6c05791522b3aecafbafc5f38a0b0c487f753d9f
Byaxy <charlesbyaxy@gmail.com>
2024-12-29 11:31:06 +0000

    updated cv and projects

--- lib/data.ts
+++ lib/data.ts
@@ -1,9 +1,10 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaJava, FaReact } from "react-icons/fa";
-import hoobankImg from "@/public/hoobank.png"
-import primegenebiomedicalsolutions from "@/public/primegenebiomedicalsolutions.png"
-import gpt3 from "@/public/gpt3.png"
+import hoobankImg from "@/public/hoobank.png";
+import primegenebiomedicalsolutions from "@/public/primegenebiomedicalsolutions.png";
+import gpt3 from "@/public/gpt3.png";
+import cverify from "@/public/cverify.png";
 
 export const links = [
   {
@@ -37,7 +38,11 @@ export const experiencesData = [
     title: "Software Developer",
     location: "Monrovia, Liberia",
     company: "Tracom Liberia Inc.",
-    description:["Took part in implementing a tax collection system for the Liberia Revenue Authority.", "Handle the Electronic Fiscal Devices integration with the MILIKI system to facilitate taxpayer invoice monitoring.", "Debug and resolve any software-related issues with deployed Electronic Fiscal Devices to the taxpayer business area."],
+    description: [
+      "Took part in implementing a tax collection system for the Liberia Revenue Authority.",
+      "Handle the Electronic Fiscal Devices integration with the MILIKI system to facilitate taxpayer invoice monitoring.",
+      "Debug and resolve any software-related issues with deployed Electronic Fiscal Devices to the taxpayer business area.",
+    ],
     icon: React.createElement(FaJava),
     date: "Mar, 2023 - Present",
   },
@@ -45,7 +50,11 @@ export const experiencesData = [
     title: "Information Technology Officer",
     location: "Monrovia, Liberia",
     company: "Fidelity Healthcare Services Inc.",
-    description: ["Developed and led the implementation of the hospital website for long-term strategic plans and solutions.", "Supported the implementation and training of staff on the hospital management system. Conducted training sessions, created user guides, and provided personalized support to ensure high user adoption of the new system. Received positive feedback from management and staff.", "Provided technical support and leadership on a variety of hospital activities requiring expertise in Information Technology."],
+    description: [
+      "Developed and led the implementation of the hospital website for long-term strategic plans and solutions.",
+      "Supported the implementation and training of staff on the hospital management system. Conducted training sessions, created user guides, and provided personalized support to ensure high user adoption of the new system. Received positive feedback from management and staff.",
+      "Provided technical support and leadership on a variety of hospital activities requiring expertise in Information Technology.",
+    ],
     icon: React.createElement(FaReact),
     date: "Feb, 2022 - Jan, 2023",
   },
@@ -53,7 +62,10 @@ export const experiencesData = [
     title: "Enrollment Officer",
     location: "Kampala, Uganda",
     company: "Uganda Electoral Commission",
-    description: ["Reviewed voter’s application forms, recognizing and promptly addressing discrepancies.", "Registered new voters by inputting their bio-metric data into the Voter Registration System promptly."],
+    description: [
+      "Reviewed voter’s application forms, recognizing and promptly addressing discrepancies.",
+      "Registered new voters by inputting their bio-metric data into the Voter Registration System promptly.",
+    ],
     icon: React.createElement(CgWorkAlt),
     date: "Nov, 2019 - Dec, 2019",
   },
@@ -61,11 +73,13 @@ export const experiencesData = [
     title: "Data Assistant",
     location: "Kampala, Uganda",
     company: "Kampala Capital City Authority",
-    description: ["Created and managed property files, recognizing and promptly addressing discrepancies.","On-boarded new properties by inputting their data into the Computer-Aided Mass Valuation Database system."],
+    description: [
+      "Created and managed property files, recognizing and promptly addressing discrepancies.",
+      "On-boarded new properties by inputting their data into the Computer-Aided Mass Valuation Database system.",
+    ],
     icon: React.createElement(CgWorkAlt),
     date: "Sept, 2018 - Oct, 2019",
   },
-  
 ] as const;
 
 export const projectsData = [
@@ -73,19 +87,35 @@ export const projectsData = [
     title: "Prime Gene Biomedical Solutions",
     description:
       "I am serving as a web developer for this project, which is website for Prime Gene Biomedical Solutions. They are suppliers of biomedical equipment, reagents, and solutions. I handled the design of the website though mainly dictated by the client. The website is hosted on Hostinger. Please note that some parts of the website are still under development.",
-    tags: ["React", "React Router DOM", "Tailwind CSS", "react-i18next", "Hostinger", "Next UI", "Swiper"],
+    tags: [
+      "React",
+      "React Router DOM",
+      "Tailwind CSS",
+      "react-i18next",
+      "Hostinger",
+      "Next UI",
+      "Swiper",
+    ],
     imageUrl: primegenebiomedicalsolutions,
     demoUrl: "https://primegenebiomedicalsolutions.com/",
-    sourceCodeUrl:"https://github.com/Byaxy/primegenebiomedicalsolutions"
+    sourceCodeUrl: "https://github.com/Byaxy/primegenebiomedicalsolutions",
   },
   {
-    title: "HooBank",
+    title: "CVerify",
     description:
-      "I served as a web developer for this practice project, which is a single-page product landing page website for a bank product. The design was created by JS-Mastery using Figma, and the live website is hosted on Firebase. The website is fully responsive and visually appealing on all devices.",
-    tags: ["React", "Tailwind CSS", "Firebase", "Figma"],
-    imageUrl: hoobankImg,
-    demoUrl: "https://modern-bank-web-app.web.app/",
-    sourceCodeUrl:"https://github.com/Byaxy/bank-modern-app"
+      "I served as the lead front-end developer for this project during the Zidio Development Internship, which is a web application that simplifies certificate management and authentication. Administrators can: Upload certificate data via Excel. Manage certificates digitally Control access and verification Users can: Search and verify certificates Download PDF certificates instantly. Admin credentials: Demo email: cverify.admin@gmail.com, Demo password: 12345678, Sample certificate IDs: CERT-M42UG4OK-9F1AC7E2",
+    tags: [
+      "React",
+      "Tailwind CSS",
+      "Shadcn UI",
+      "Node.js",
+      "MongoDB",
+      "Express.js",
+      "Tanstack Query",
+    ],
+    imageUrl: cverify,
+    demoUrl: "https://cverify.vercel.app/",
+    sourceCodeUrl: "https://github.com/Byaxy/certificate-verification-system",
   },
   {
     title: "GPT-3",
@@ -94,7 +124,7 @@ export const projectsData = [
     tags: ["React", "Tailwind CSS", "Firebase", "Figma"],
     imageUrl: gpt3,
     demoUrl: "https://openai-gpt3.web.app/",
-    sourceCodeUrl:"https://github.com/Byaxy/GPT-3"
+    sourceCodeUrl: "https://github.com/Byaxy/GPT-3",
   },
 ];
 
